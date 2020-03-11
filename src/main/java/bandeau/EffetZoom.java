2337203b4874a785bb5742256a0fa51010c92ed2
nferrer <nferrer@172.16.18.216>
2020-03-11 14:08:49 +0100

    add effetPolice, changes in EffetTourne, ExempeDUtilisation, Scenario1, EffetZoom

--- src/main/java/bandeau/EffetZoom.java
+++ src/main/java/bandeau/EffetZoom.java
@@ -4,10 +4,24 @@ import java.awt.Font;
 
 public class EffetZoom extends Effet {
 
+	/**
+	 * Méthode par défaut
+	 * @param monBandeau bandeau à changer
+	 */
     public void lancer(Bandeau monBandeau) {
-        monBandeau.setMessage("Zoom........");
-	monBandeau.sleep(1000);
         for (int i = 5; i < 60; i += 5) {
+        	monBandeau.setFont(new Font("Dialog", Font.BOLD, 5 + i));
+        	monBandeau.sleep(100);
+        }
+    }
+    
+    /**
+     * Méthode avec param
+     * @param monBandeau bandeau à changer
+     * @param tailleDuZoom taille en unité de grandeur du zoom
+     */
+    public void lancer(Bandeau monBandeau, int tailleDuZoom) {
+    	for (int i = 5; i < tailleDuZoom; i += 5) {
         	monBandeau.setFont(new Font("Dialog", Font.BOLD, 5 + i));
         	monBandeau.sleep(100);
         }
--- src/main/java/bandeau/Scenario1.java
+++ src/main/java/bandeau/Scenario1.java
@@ -13,11 +13,16 @@ public class Scenario1 extends Scenario {
     
     private final EffetZoom zoom = new EffetZoom();
     private final EffetTourne tourne = new EffetTourne();
+    private final EffetPolice police = new EffetPolice();
+    
 	@Override
 	public void lancer(Bandeau monBandeau) {
             
             zoom.lancer(monBandeau);
-		
+            monBandeau.sleep(1000);
+            tourne.lancer(monBandeau);
+            monBandeau.sleep(100);
+            police.lancer(monBandeau);
 		
 	}
 
