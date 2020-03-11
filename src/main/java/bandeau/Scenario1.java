06afdec998a2b809dc53538921e49f0452616118
alefev02 <alefev02@172.16.18.187>
2020-03-11 14:19:51 +0100

    Premier scénario fini

--- src/main/java/bandeau/Scenario1.java
+++ src/main/java/bandeau/Scenario1.java
@@ -14,13 +14,19 @@ public class Scenario1 extends Scenario {
     private final EffetZoom zoom = new EffetZoom();
     private final EffetTourne tourne = new EffetTourne();
     private final EffetPolice police = new EffetPolice();
+    private final EffetCouleurTexte texte = new EffetCouleurTexte();
+    private final EffetCouleurFond fond = new EffetCouleurFond();
     
 	@Override
 	public void lancer(Bandeau monBandeau) {
             
             zoom.lancer(monBandeau);
             monBandeau.sleep(1000);
+            texte.lancer(monBandeau);
+            monBandeau.sleep(100);
             tourne.lancer(monBandeau);
+            monBandeau.sleep(100);
+            fond.lancer(monBandeau);
             monBandeau.sleep(100);
             police.lancer(monBandeau);
 		
